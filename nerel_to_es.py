f389aa5f90cfd3fbafacebd5a30d32ffc6bec127
Your Name <you@example.com>
2023-02-12 14:28:09 +0300

    подготовлен код до сбора для эластика

--- nerel_to_es.py
+++ nerel_to_es.py
@@ -6,14 +6,42 @@ import elasticsearch
 from elasticsearch import helpers
 
 def read_txt_file(file_path):
+    text_data = []
+
     with open(file_path, 'r') as f:
-        print(f.read())
+        lines = f.readlines()
+        for line in lines:
+            if len(line) is not 0:
+                text_data.append(line)
+
+    return text_data
+
 
 def read_ann_file(file_path):
+    text_meta = {}
     with open(file_path, 'r') as f:
-        print(f.read())
+        lines = f.readlines()
+        for line in lines:
+            if len(line) is not 0:
+                parts_of_line = line.split(' ')
+                if line.startswith("T"):
+                    pos = []
+                    for info in enumerate(parts_of_line):
+                        if parts_of_line[info].isdigit():
+                            pos.append(int(parts_of_line[info], parts_of_line[info+1]))
+                        meta_data[parts_of_line[0]] = [pos]
+                        meta_data[parts_of_line[0]].append[" ".join(parts_of_line[info+2:])]
+                        break
+                if line.startswith("N"):
+                    for info in enumerate(parts_of_line):
+                        if parts_of_line[info].startswith("T"):
+                            if parts_of_line[info+1].startswith("Wikidata:") and \
+                                    parts_of_line[info+1].lstrip("Wikidata:") is not "NULL":
+                                meta_data[parts_of_line[info]] = \
+                                    [meta_data[parts_of_line[info]], parts_of_line[info+1].lstrip("Wikidata:")]
+                                break
 
-    return id_pos
+    return text_meta
 
 
 def dump(args):
@@ -30,11 +58,16 @@ def dump(args):
         if file.endswith(".txt"):
             try:
                 ann_file = file.rstrip(".txt") + ".ann"
-                text_meta = read_ann_file(ann_file)
                 text_data = read_txt_file(file)
+                text_meta = read_ann_file(ann_file)
             except Exception as e:
                 print(e)
                 continue
+        for record in text_meta:
+            for record_text in text_data:
+                if record_text[record[0][0]:record[0][1]] == record[1]:
+                    pass
+
 
 
 def parse_args():
