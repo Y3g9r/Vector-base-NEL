c68672e6ae89fc0eed2c67722a46af3aa7b5e9f1
Your Name <you@example.com>
2023-02-15 20:42:00 +0300

    подготовлен код который возвращает словари сущностей и позиций сущностей, перерефакторино до класса

--- src/entity_extractor/entity_extractor.py
+++ src/entity_extractor/entity_extractor.py
@@ -1,16 +1,48 @@
-import os
-os.environ['CUDA_VISIBLE_DEVICES']='cpu'
-
-from deeppavlov import build_model
 from razdel import sentenize
+from natasha import Segmenter, NewsEmbedding, NewsNERTagger, Doc
 
-text = "Алексей А.В. из ФСБ и Пётр М.Е из МВД пошли к реке. Никто не видел Артёма.. . Но все знали, что он рядом!"
+#self.text = "Алексей А.В. из Артёмовска и Пётр М.Е из Магадана пошли к реке. Никто не видел Артёма.. . Но все знали, что он рядом!"
 
-sentences = list(sentenize(text))
-sentences = [list(sentence)[2] for sentence in sentences]
 
-ner_model = build_model('ner_collection3_bert', download=False)
+class EntityExtractor():
+    def __init__(text: str):
+        self.text = text
+        self.sentences = list(sentenize(text))
+        self.sentences = [list(sentence)[2] for sentence in sentences]
 
-sentences_count = len(sentences)
-sentences_entities = ner_model(sentences)
+        self.segmenter = Segmenter()
+        self.emb = NewsEmbedding()
+        self.ner_tagger = NewsNERTagger(emb)
 
+        self.entity_positions = {}
+        self.entity_text = {}
+
+    def _apply_tag_ner(self, sentece: str):
+        doc = Doc(sentence)
+        doc.segment(self.segmenter)
+        doc.tag_ner(self.ner_tagger)
+
+        return doc
+
+    def get_entitys_positions(self):
+        for sentence in self.sentences:
+            doc = self._apply_tag_ner(sentence)
+
+            self.entity_positions[sentences.index[sentence]] = []
+            for span in doc.spans:
+                entity_positions.append([span.start, span.stop])
+
+        return entity_positions
+
+    def get_entitys_text(self):
+        for sentence in self.sentences:
+            doc = self._apply_tag_ner(sentence)
+
+            self.entity_text[sentences.index[sentence]] = []
+            for span in doc.spans:
+                entity_text.append(span.text)
+
+        return entity_text
+
+
+print(doc.spans[0])
