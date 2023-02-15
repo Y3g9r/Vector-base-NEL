22c1786913b36d5e07444b2507053d82e523b9b9
Your Name <you@example.com>
2023-02-15 18:16:55 +0300

    эксперементирование с выделением сущностей

--- src/entity_extractor/entity_extractor.py
+++ src/entity_extractor/entity_extractor.py
@@ -4,7 +4,7 @@ os.environ['CUDA_VISIBLE_DEVICES']='cpu'
 from deeppavlov import build_model
 from razdel import sentenize
 
-text = "Алексей А.В. и Пётр М.Е пошли к реке. Никто не видел Артёма.. . Но все знали, что он рядом!"
+text = "Алексей А.В. из ФСБ и Пётр М.Е из МВД пошли к реке. Никто не видел Артёма.. . Но все знали, что он рядом!"
 
 sentences = list(sentenize(text))
 sentences = [list(sentence)[2] for sentence in sentences]
