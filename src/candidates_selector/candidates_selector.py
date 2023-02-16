5e5047ca33570a062bf3125d327b3659357dd02b
Your Name <you@example.com>
2023-02-16 13:43:11 +0300

    переработана функция обращения к эластику по получению именованных сущностей

--- src/candidates_selector/candidates_selector.py
+++ src/candidates_selector/candidates_selector.py
@@ -5,7 +5,6 @@ import elasticsearch
 class CandidatesSelector():
 
     def __init__(self, es_host, candidates_limit: int = 5):
-        print(IP)
         self._morph = pymorphy2.MorphAnalyzer(lang='ru')
         self.candidates_limit = candidates_limit
         self.es = elasticsearch.Elasticsearch(es_host)
@@ -19,16 +18,20 @@ class CandidatesSelector():
             normalized_entitys_list.append(self._morph.parse(entity)[0].normal_form)
         return normalized_entitys_list
 
-    def _get_candidates_from_elastic(self, entity: str):
-        body = {
-            "query": {
-                "match": {
-                    "labels.ru": entity
+    def _get_candidates_from_elastic(self, entitys_list: list):
+        entity_candidates = []
+        for entity in entitys_list:
+            body = {
+                "query": {
+                    "match": {
+                        "labels.ru": entity
+                    }
                 }
             }
-        }
+            entity_candidates.append([self.es.search(index=self.index, doc_type=self.type,\
+                                                    body=body, size=self.candidates_limit)])
 
-        return self.es.search(index=self.index, doc_type=self.type, body=body, size = self.candidates_limit)
+        return entity_candidates
 
 
     def get_candidates(self, sentences_entitys_list: list):
@@ -36,11 +39,10 @@ class CandidatesSelector():
         sentence_candidates = []
 
         for sentence_entitys in sentences_entitys_list:
-            for entity in sentence_entitys:
-                sentence_candidates_raw = self._get_candidates_from_elastic(\
-                    self._entity_normalizer(entity))
-                for sentence in sentence_candidates_raw:
-                    sentence_candidates.append()
+            sentence_candidates_raw = self._get_candidates_from_elastic(\
+                self._entity_normalizer(sentence_entitys))
+            for sentence in sentence_candidates_raw:
+                sentence_candidates.append(sentence[][][])
 
 
 
