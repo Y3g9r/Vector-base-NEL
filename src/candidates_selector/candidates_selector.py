606615e6125326428035db8fcafecc39bb74e670
Your Name <you@example.com>
2023-02-16 14:17:26 +0300

    есть первый вид в получении кандидатов, однако есть и баги

--- src/candidates_selector/candidates_selector.py
+++ src/candidates_selector/candidates_selector.py
@@ -23,29 +23,47 @@ class CandidatesSelector():
         for entity in entitys_list:
             body = {
                 "query": {
-                    "match": {
-                        "labels.ru": entity
+                    "bool":{
+                        "must": [{
+                            "match": {
+                                "labels.ru": entity
+                            }}, {
+                            "exists": {
+                                "field": "descriptions.ru"
+                            }
+                        }],
+                        "must_not": [{
+                            "match": {
+                                "descriptions.ru": "страница значений"
+                            }
+                        }]
                     }
                 }
             }
-            entity_candidates.append([self.es.search(index=self.index, doc_type=self.type,\
-                                                    body=body, size=self.candidates_limit)])
+            entity_candidates.append([self.es.search(index=self.index, doc_type=self.type, body=body, size=self.candidates_limit)])
 
         return entity_candidates
 
 
     def get_candidates(self, sentences_entitys_list: list):
-        sentence_candidates_raw = []
-        sentence_candidates = []
+        all_candidates = []
 
         for sentence_entitys in sentences_entitys_list:
+            sentence_candidates_raw = []
             sentence_candidates_raw = self._get_candidates_from_elastic(\
                 self._entity_normalizer(sentence_entitys))
-            for sentence in sentence_candidates_raw:
-                sentence_candidates.append(sentence[][][])
+            sentence_candidates = []
+            for candidates in sentence_candidates_raw:
+                entity_candidates = []
+                for entity in candidates[0]["hits"]["hits"]:
+                    entity_candidates.append(entity["_source"]["descriptions"]["ru"])
+                sentence_candidates.append(entity_candidates)
+            all_candidates.append(sentence_candidates)
+
+        return all_candidates
 
 
 
 sentence_entitys_list = [["Артёма",'Организация объединённых наций'],['МВД']]
 candidate_selector = CandidatesSelector(IP)
-candidate_selector.get_candidates(sentence_entitys_list)
+print(candidate_selector.get_candidates(sentence_entitys_list))
